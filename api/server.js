58b44468416607321384306ff6c2604e5d7d0e0f
amichalo421 <andre.michalowski@gmail.com>
2021-07-11 16:23:35 -0400

    Basic router setup

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express"); 
+const questionsRouter = require('../routers/questions-router.js');
+
 const server = express();
 server.use(express.json());
+
+server.use('/api/questions/', questionsRouter);
+
 
 server.get('/', (req, res) => {
 	res.status(200).json({ serverMessage: 'Test endpoint' });
---
+++ routers/questions-router.js
@@ -0,0 +1,12 @@
+const express = require('express') // import express
+//import modal or db here
+const questionsRouter = express.Router();// instantiate router
+
+//(insert api routes here (with abbreviated url))
+// questionsRouter.get('/', (req, res) => {...}
+// questionsRouter.get('/:id', (req, res) => {...}
+// questionsRouter.post('/', (req, res) => {...}
+// questionsRouter.delete('/:id', (req, res) => {...}
+// questionsRouter.put('/:id', (req, res) => {...}
+
+module.exports = questionsRouter; //export default router;
