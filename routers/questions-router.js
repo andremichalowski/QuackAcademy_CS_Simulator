5d44b53465aa8e4eeb1024ab46e169c97db2d6af
amichalo421 <andre.michalowski@gmail.com>
2021-07-11 19:40:52 -0400

    Router (questions-router.js)

--- routers/questions-router.js
+++ routers/questions-router.js
@@ -1,12 +1,88 @@
-const express = require('express') // import express
+const express = require('express') 
 //import modal or db here
-const questionsRouter = express.Router();// instantiate router
+const questionsRouter = express.Router();
 
-//(insert api routes here (with abbreviated url))
-// questionsRouter.get('/', (req, res) => {...}
-// questionsRouter.get('/:id', (req, res) => {...}
-// questionsRouter.post('/', (req, res) => {...}
-// questionsRouter.delete('/:id', (req, res) => {...}
-// questionsRouter.put('/:id', (req, res) => {...}
 
-module.exports = questionsRouter; //export default router;
+questionsRouter.get("/", (req, res) => {
+  Items.getAll()
+    .then((questions) => {
+      res.json(questions);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get questions" });
+    });
+});
+
+questionsRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Items.getById(id)
+    .then((question) => {
+      if (question) {
+        res.json(question);
+      } else {
+        res.status(404).json({ message: "Could not find question with given id." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get question" });
+    });
+});
+
+questionsRouter.post("/", (req, res) => {
+  const questionData = req.body;
+
+  Items.add(questionData)
+    .then((question) => {
+      res.status(201).json({ created: question });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to create new question" });
+    });
+});
+
+questionsRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  Questions.update(id, changes)
+    .then((question) => {
+      if (question) {
+        res.json({ updated: question });
+      } else {
+        res.status(404).json({ message: "Could not find question with given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to update question" });
+    });
+});
+
+questionsRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Questions.remove(id)
+    .then((question) => {
+      if (question) {
+        res.json({ removed: question });
+      } else {
+        res.status(404).json({ message: "Could not find question with given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to delete question" });
+    });
+});
+
+// questionsRouter.get("/:id/secondaries", (req, res) => {
+//   Questions.getQuestionSecondaries(req.params.id)
+//     .then((secondaries) => {
+//       res.status(200).json({ data: secondaries });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ message: err.message });
+//     });
+// });
+
+
+module.exports = questionsRouter;
