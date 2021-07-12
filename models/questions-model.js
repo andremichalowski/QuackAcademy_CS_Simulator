77db3d1c2033b15035e3594aad33df47010e79d6
amichalo421 <andre.michalowski@gmail.com>
2021-07-11 20:17:00 -0400

    Model (questions-model.js)

---
+++ models/questions-model.js
@@ -0,0 +1,41 @@
+const db = require('../../data/dbConfig');
+
+
+
+module.exports = {
+    getAll,
+		getById,
+    add,
+    update,
+    remove,
+    getUserPosts,
+};
+
+function getAll() {
+    return db("questions");
+}
+
+function getById(id) {
+    return db("questions").where({ id }).first();
+}
+
+function add(user) {
+    return db("questions")
+        .insert(question, "id")
+        .then(ids => {
+            const id = ids[0];
+            return getById(id);
+        });
+}
+
+function update(id, changes) {
+    return db("questions").where({ id }).update(changes);
+}
+
+function remove(id) {
+    return db("questions").where({ id }).del();
+}
+
+function getUserPosts(id) {
+    return db("questions").where("question_id", id);
+}
