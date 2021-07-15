5cb79a9f4c7654331b9c016c324be579bde4e6b0
amichalo421 <andre.michalowski@gmail.com>
2021-07-15 19:33:18 -0400

    Dotenv added

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+require("dotenv").config();
 const server = require('./api/server.js');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const secret = process.env.TEMP_SECRET || 'FOO SECRET'
+
+console.log(PORT, secret);
+
 server.listen(PORT, () => {console.log(`Server running on localhost:${PORT}`)})
