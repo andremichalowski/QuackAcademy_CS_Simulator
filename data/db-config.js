04105761ab0c7c2ada01fc3776a4c3fbe0495135
amichalo421 <andre.michalowski@gmail.com>
2021-07-18 19:46:20 -0400

    Heroku pg changes (db-config)

--- data/db-config.js
+++ data/db-config.js
@@ -1,3 +1,12 @@
+// DEVELOPMENT
+// const knex = require('knex');
+// const knexfile = require('../knexfile.js');
+
+// const environment = process.env.NODE_ENV || 'development'; 
+// const config = knexfile[environment];
+
+// module.exports = knex(config)
+
 const knex = require('knex');
 const knexfile = require('../knexfile.js');
 
