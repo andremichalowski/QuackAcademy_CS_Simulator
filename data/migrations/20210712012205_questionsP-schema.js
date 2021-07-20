85a6fc46306c1fac2a019c1d926e229553b8fca9
amichalo421 <andre.michalowski@gmail.com>
2021-07-19 22:14:00 -0400

    Migration table name update add capital 'P'

--- data/migrations/20210712012205_questionsP-schema.js
+++ data/migrations/20210712012205_questionsP-schema.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('questionsp', tbl => {
+  return knex.schema.createTable('questionsP', tbl => {
     tbl.increments(); 
     tbl.string('name').notNullable();
     tbl.string('question').notNullable();
@@ -13,5 +13,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('questionsp');
+  return knex.schema.dropTableIfExists('questionsP');
 };
